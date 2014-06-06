e473e57fb0ba981354950f5e4ea115d80b4ee0c8
Richard Checnes <richardchecnes@hotmail.com>
2014-06-06 18:59:52 -0500

    archvios para funciones creado en intimedia

---
+++ src/FuncionesController.php
@@ -0,0 +1,15 @@
+<?php
+namespace Loque;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class FuncionesController
+{
+    public function registrarAction(Request $request)
+    {
+
+        return "Maria parado de bellido";
+    }
+}
+
+?>
